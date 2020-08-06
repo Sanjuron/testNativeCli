163bef774d055869031a7f1087f3975de6645f95
Sanjuron <julien_nn@live.fr>
2020-08-06 11:12:50 +0200

    reunion thomas moi

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Button, Input, ThemeProvider } from "react-native-elements";
+import { StyleSheet, Text, View, Alert } from "react-native";
+import Icon from 'react-native-vector-icons/FontAwesome'; // thomas
+import { Button, Input, ThemeProvider, CheckBox, Header, ScrollView, Picker } from "react-native-elements";
 
 /// Import from 'react-native-pdf-lib'
 // import PDFLib, {  PDFDocument, PDFPage } from 'react-native-pdf-lib';
@@ -382,21 +383,56 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider>
-      <View style={styles.container}>
-        <Text style={styles.h1}>Contrat de Location</Text>
-        <Input placeholder="Nom du demandeur" onChangeText={changeLastname} />
-        <Input
-          placeholder="Prénom du demandeur"
-          onChangeText={changeFirstame}
-        />
-        <Input
-          placeholder="Modèle de la voiture à louer"
-          onChangeText={changeModel}
-        />
-        <Button title="sauvegarder pdf" onPress={createPdf} />
-      </View>
-    </ThemeProvider>
+    <ScrollView>
+        <View style={styles.container}>
+          <Header centerComponent={{ text: 'Garage Solidaire', style: { color: '#fff' }}}/>
+          <View>
+            <CheckBox center title='Monsieur' />
+            <CheckBox center title='Madame' />
+          </View>
+          <Input style={styles.firstInput} placeholder='Nom...' leftIcon= {<Icon name='user'size={17}color='#7B8894'/>}/>
+          <Input placeholder='Prénom...' leftIcon= {<Icon name='user'size={17}color='#7B8894'/>}/>
+          <Input placeholder='Date de naissance...' leftIcon= {<Icon name='calendar'size={17}color='#7B8894'/>}/>
+          <Input placeholder='Adresse...' leftIcon= {<Icon name='home'size={17}color='#7B8894'/>}/>
+          <Input placeholder='Permis...' leftIcon= {<Icon name='id-card'size={17}color='#7B8894'/>}/>
+          <Input placeholder='Contrat de travail...' leftIcon= {<Icon name='pencil-square-o'size={17}color='#7B8894'/>}/>
+          <View>
+            <CheckBox center title='QPV'/>
+            <CheckBox center title='RSA' />
+          </View>
+          <Input placeholder="Carte d'identité..." leftIcon= {<Icon name='address-card'size={17}color='#7B8894'/>}/>
+          <Input placeholder='Empreinte banquaire...' leftIcon= {<Icon name='credit-card-alt'size={17}color='#7B8894'/>}/>
+          {/* <Picker
+              style={{ height: 50, width: '100%', marginTop: 10, marginBottom: 10}}
+              onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}>
+              <Picker.Item label="Marque" value="mode" />
+              <Picker.Item label="Peugeot" value="pt" />
+              <Picker.Item label="Renault" value="rt" />
+          </Picker>
+          <Picker
+              style={{ height: 50, width: '100%', marginTop: 10, marginBottom: 10}}
+              onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}>
+              <Picker.Item label="Modèle" value="mode" />
+              <Picker.Item label="106" value="p106" />
+              <Picker.Item label="206" value="p206" />
+          </Picker>
+          <Input placeholder='Immatriculation...' leftIcon= {<Icon name='bars'size={17}color='#7B8894'/>}/>
+          <Picker
+              style={{ height: 50, width: '100%', marginTop: 10, marginBottom: 10 }}
+              onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}>
+              <Picker.Item label="Etat" value="mode" />
+              <Picker.Item label="Bon état" value="bon" />
+              <Picker.Item label="Etat correct" value="correct" />
+              <Picker.Item label="Mauvais état" value="mauvais" />
+          </Picker> */}
+          <Input placeholder='Kilométrage jour...' leftIcon= {<Icon name='tachometer'size={17}color='#7B8894'/>}/>
+          <Input placeholder='Date de départ...' leftIcon= {<Icon name='map-o'size={17}color='#7B8894'/>}/>
+          <Input placeholder='Date de retour...' leftIcon= {<Icon name='share'size={17}color='#7B8894'/>}/>
+        </View>
+        <View>
+            <Button title="OBTENIR MON PDF" onPress={createPdf}/>
+        </View>
+      </ScrollView>
   );
 }
 
